f9a224c77cb1df124511a0637ce12a08e46ebd80
Đào Vũ Đạt <dat.daovu@gmail.com>
2024-01-06 00:51:34 +0700

    fix ReactPlayer props

--- src/components/features/VideoPlayer.tsx
+++ src/components/features/VideoPlayer.tsx
@@ -14,8 +14,9 @@ export function VideoPlayer() {
       {hasWindow && (
         <ReactPlayer
           url="./video/intro_video.webm"
-          playing
-          loop
+          playing={true}
+          loop={true}
+          muted={true}
           width="100%"
           height="100%"
           controls={false}
